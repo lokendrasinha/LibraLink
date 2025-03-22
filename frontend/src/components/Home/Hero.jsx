4c6a2d283dabf16d797639a17d33b0118d8fac7d
lokendrasinha <lokendrasinha2003@gmail.com>
2025-03-23 03:01:58 +0530

    updated

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] flex flex-col md:flex-row items-center justify-center">
-      <div className='w-full mb-12 md:mb-0 lg:w-3/6 flex flex-col lg:items-start justify-center'><h1 className='text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left'>
-      Books You Love, Delivered!
-        </h1>
-        <p className="mt-4 text-xl text-zinc-300 text-center lg:text-left">Unmissable stories, unbeatable prices—start reading today!</p>
-        <div className='mt-8 flex justify-center'>
-        <Link to="/all-books" className='text-yellow-100  font-semibold border border-yellow-100 text-xl lg:text-2xl px-10 py-3 hover:bg-zinc-800 rounded-full '>Uncover Reads</Link>
-        </div>
-        </div>
-      <div className='w-full lg:w-3/6 h-auto lg:h-[100%] flex items-center justify-center'></div>
-      <img src="https://hancockghostwriters.com/assets/img/non-fiction/non-fiction.png" alt="hero" className="src" />
+    <div className='h-[75vh] flex'>
+      <div className='w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center'>
+      <h1 className='text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left'>Discover Your Next Great Read</h1>
+      <p className='mt-4 text-xl text-zinc-300 text-center lg:text-left'>Uncover captivating stories, enriching knowledge, and endless inspiration in our curated collection of books</p>
+      <div className='mt-8'>
+      <button className='text-yellow-100 text:xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full'>Discover Books</button>
+      </div>
+      </div>
+      <div className='w-full lg:w-3/6'>
+        <img src="./library-concept-illustration.png" alt="hero image" />
+      </div>
     </div>
   )
 }
