e4cc4a5e4c2bafc7c3620bfce07c3b66f78296d9
lokendrasinha <lokendrasinha2003@gmail.com>
2025-03-23 03:03:31 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,10 @@
-import React from 'react'
-import Hero from "../components/Home/Hero";
-import RecentlyAdded from '../components/Home/RecentlyAdded';
+import React from 'react';
+import Hero from '../components/Home/Hero';
 
 const Home = () => {
-  return (
-    <div className=' bg-zinc-900 text-white px-10 py-8 ' >
-      <Hero />
-      <RecentlyAdded/>
-    </div>
-  );
+  return <div className="bg-zinc-900 text-white px-10 py-8">
+    <Hero/>
+  </div>
 };
 
-export default Home;
+export default Home
